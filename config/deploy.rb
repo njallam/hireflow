78cb5a4cf4655266d0ab626bd1efacc2b215524b
Nicholas Allam <njallam@outlook.com>
2018-05-30 11:42:58 +0100

    Use CI credentials to get repo for CD

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,13 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 
 set :pty, true
 
+if ENV.key?('CI')
+  set :repo_url, ENV['CI_REPOSITORY_URL']
+  set :branch, ENV['CI_COMMIT_SHA']
+else
+  set :ssh_options, forward_agent: true
+end
+
 set :linked_files, %w[config/master.key]
 set :linked_dirs, fetch(:linked_dirs, [])
   .push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets',
