49cec195db627dd04fc1320ce77bcdec644d6b2d
Benjamin Dimant <benjamin.dimant15@imperial.ac.uk>
2018-06-02 14:48:03 +0100

    [all] Write test cases to handle business controller logic for editing and updating

--- test/controllers/businesses_controller_test.rb
+++ test/controllers/businesses_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class BusinessesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @business = build :business
+  end
+
+  test 'should allow editing business profile' do
+    sign_in @business
+    get edit_business_profile_url
+    assert_response :success
+  end
+
+  test 'should allow updating business profile' do
+    sign_in @business
+    new_name = Faker::Company.name
+    patch business_profile_url, params: { business: { name: new_name } }
+    assert_equal @business.name, new_name
+    assert_redirected_to edit_business_profile_path
+  end
 end
