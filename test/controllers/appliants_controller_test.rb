6ffba1fd217be84efd542e567f3c5c24d25b489d
Benjamin Dimant <benjamin.dimant15@imperial.ac.uk>
2018-06-02 14:55:45 +0100

    [all] Fix haml-lint error with line being too long

--- test/controllers/appliants_controller_test.rb
+++ test/controllers/appliants_controller_test.rb
@@ -15,7 +15,8 @@ class AppliantsControllerTest < ActionDispatch::IntegrationTest
     sign_in @applicant
     new_first_name = Faker::Name.first_name
     new_last_name = Faker::Name.last_name
-    patch applicant_profile_url, params: { applicant: { first_name: new_first_name, last_name: new_last_name } }
+    patch applicant_profile_url, params: { applicant: { first_name: new_first_name,
+                                                        last_name: new_last_name } }
     assert_equal @applicant.first_name, new_first_name
     assert_equal @applicant.last_name, new_last_name
     assert_redirected_to edit_applicant_profile_path
