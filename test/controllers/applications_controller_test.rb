c640f58c335ffa2440d2a3f2b1fdaae5d17ec5d9
Nicholas Allam <njallam@outlook.com>
2018-06-03 15:00:18 +0100

    Add test for applications#show

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -4,24 +4,30 @@ class ApplicationsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @applicant = create :applicant
     @job = create :job
+    @application = create :application
     sign_in @applicant
   end
 
   test 'should be able to apply to job' do
     assert_difference 'Application.count', +1 do
-      post apply_job_url(@job)
+      post apply_job_url @job
       assert_redirected_to @applicant.applications.last
     end
   end
 
   test 'should not be able to apply to same job twice' do
     assert_difference 'Application.count', +1 do
-      2.times { post apply_job_url(@job) }
+      2.times { post apply_job_url @job }
     end
   end
 
   test 'should show all applications for an applicant' do
     get applications_path
+    assert_response :success
+  end
+
+  test 'should show the status of an application for an applicant' do
+    get application_path @application
     assert_response :success
   end
 end
