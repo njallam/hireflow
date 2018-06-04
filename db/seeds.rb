9b633193f6bce9dc7c386f09e08fb88810c46653
Nicholas Allam <njallam@outlook.com>
2018-06-04 12:30:14 +0100

    Add applicant name to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 business1 = Business.create email: 'business1@example.com', password: '123456', password_confirmation: '123456', name: 'Example Corp'
 business2 = Business.create email: 'business2@example.com', password: '123456', password_confirmation: '123456', name: 'Dummy Co.'
 
-Applicant.create email: 'applicant@example.com', password: '123456', password_confirmation: '123456'
+Applicant.create email: 'applicant@example.com', password: '123456', password_confirmation: '123456', first_name: 'Bob', last_name: 'Smith'
 
 Job.create position: 'Frontend developer', description: 'Do the frontend stuff', business: business1
 Job.create position: 'Backend developer', description: 'Do the backend stuff', business: business1
