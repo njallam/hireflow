a0434ed418fe4cac324c44344ec6704cc4e5a351
Lewis Treacy <lewisjtreacy@gmail.com>
2018-06-06 15:56:05 +0100

    Update factories and seeds for applicants and businesses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
-business1 = Business.create email: 'business1@example.com', password: '123456', password_confirmation: '123456', name: 'Example Corp'
-business2 = Business.create email: 'business2@example.com', password: '123456', password_confirmation: '123456', name: 'Dummy Co.'
+business1 = Business.create email: 'business1@example.com', password: '123456', password_confirmation: '123456', name: 'Example Corp', city: 'London'
+business2 = Business.create email: 'business2@example.com', password: '123456', password_confirmation: '123456', name: 'Dummy Co.', city: 'Tring'
 
-Applicant.create email: 'applicant@example.com', password: '123456', password_confirmation: '123456', first_name: 'Bob', last_name: 'Smith'
+applicant = Applicant.create email: 'applicant@example.com', password: '123456', password_confirmation: '123456', first_name: 'Bob', last_name: 'Smith'
 
-Job.create position: 'Frontend developer', description: 'Do the frontend stuff', business: business1
+job1 = Job.create position: 'Frontend developer', description: 'Do the frontend stuff', business: business1
 Job.create position: 'Backend developer', description: 'Do the backend stuff', business: business1
 
-Job.create position: 'Data Scientist', description: 'Look at all the numbers and do some machine learning', business: business2
+job2 = Job.create position: 'Data Scientist', description: 'Look at all the numbers and do some machine learning', business: business2
 Job.create position: 'Sys Admin', description: 'Make the computers work', business: business2
+
+Application.create job: job1, applicant: applicant
+Application.create job: job2, applicant: applicant
--- test/factories/businesses.rb
+++ test/factories/businesses.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     password 'password'
     password_confirmation 'password'
     name { Faker::SiliconValley.company }
+    city { Faker::Address.city }
+    description { Faker::SiliconValley.motto }
   end
 end
