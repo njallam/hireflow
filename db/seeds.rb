74a764b6ce6e88c7bde48e0626fe7438e7999d78
Lewis Treacy <lewisjtreacy@gmail.com>
2018-06-07 16:27:31 +0100

    add seeding for achievements and work experience

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ business1 = Business.create email: 'business1@example.com', password: '123456', 
 business2 = Business.create email: 'business2@example.com', password: '123456', password_confirmation: '123456', name: 'Dummy Co.', city: 'Tring'
 
 applicant = Applicant.create email: 'applicant@example.com', password: '123456', password_confirmation: '123456', first_name: 'Bob', last_name: 'Smith'
+Achievement.create title: 'Duke of Edinburgh', date: '2012-05-15', applicant: applicant
+Experience.create place: 'BidFX', title: 'Code monkey', start_date: '2017-07-20', end_date: '2017-07-21', applicant: applicant
 
 job1 = Job.create position: 'Frontend developer', description: 'Do the frontend stuff', business: business1
 Job.create position: 'Backend developer', description: 'Do the backend stuff', business: business1
