8a8046a9f52d16f3ff0d00953b8b6b8e16831937
Nicholas Allam <njallam@outlook.com>
2018-06-09 12:13:37 +0100

    Partialize profile element collection rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def link_fa_to(icon_name, link, html_options = {})
+    link_to icon('fas', icon_name), link, html_options
+  end
 end
--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -6,7 +6,7 @@ class Applicant < ApplicationRecord
 
   has_many :achievements, dependent: :destroy
   has_many :experiences, dependent: :destroy
-  has_many :education, dependent: :destroy
+  has_many :educations, dependent: :destroy
   has_many :applications, dependent: :destroy
 
   def name
