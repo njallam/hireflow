2a6e979aa1ec330b3d8f938a4bd7bb77d63f0804
Benjamin Dimant <benjamin.dimant15@imperial.ac.uk>
2018-06-13 14:39:30 +0100

    Have circles render on applications in application view to show their state

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -1,34 +1,34 @@
 module ApplicationsHelper
-  def personal_color
-    'green' if @application.personaled?
+  def personal_color(application)
+    application.personaled? ? 'green' : 'yellow'
   end
 
-  def cover_color
-    'green' if @application.covered?
+  def cover_color(application)
+    'green' if application.covered?
   end
 
-  def screening_color
-    return unless @application.covered?
-    if @application.screened?
+  def screening_color(application)
+    return unless application.covered?
+    if application.screened?
       'green'
     else
-      @application.rejected? ? 'red' : 'yellow'
+      application.rejected? ? 'red' : 'yellow'
     end
   end
 
-  def interview_color
-    return unless @application.screened?
-    if @application.interviewed?
+  def interview_color(application)
+    return unless application.screened?
+    if application.interviewed?
       'green'
     else
-      @application.rejected? ? 'green' : 'yellow'
+      application.rejected? ? 'green' : 'yellow'
     end
   end
 
-  def accepted_color
-    if @application.accepted?
+  def accepted_color(application)
+    if application.accepted?
       'green'
-    elsif @application.screened? && @application.rejected?
+    elsif application.screened? && application.rejected?
       'red'
     end
   end
